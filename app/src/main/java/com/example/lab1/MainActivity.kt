b3439c88e92ab58e712a5c3f5a028bab430ed55a
Maxumka <maxumka1792@gmail.com>
2020-09-06 19:55:45 +0700

    (lab 3) Add navigation toolbar and three fragments

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -2,121 +2,75 @@ package com.example.lab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.Gravity
+import android.view.Menu
 import android.view.View
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    companion object {
-        const val stateString: String = "stateString" // ключ для сохранения переменной нормы калорий
-    }
-
-    private var mCalorie: String? = null // поле для хранения нормы калорий
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        mCalorie = textView2.text.toString()
-
-        buttonCalculate.setOnClickListener {
-            if (validation()) {
-                mCalorie = getCalculatedCalorie()
-                textView2.text = mCalorie
-            }
-        }
+        setSupportActionBar(toolbar)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fr_main, FragmentLosing())
+            .commit()
+        createDrawer()
     }
 
-    override fun onSaveInstanceState(outState: Bundle) { // метод для сохранения нормы калорий при уничтожени активности
-        super.onSaveInstanceState(outState)
-        mCalorie = textView2.text.toString()
-        outState?.putString(stateString, mCalorie)
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) { // метод для восстановления нормы калорий при уничтожени активности
-        super.onRestoreInstanceState(savedInstanceState)
-        mCalorie = savedInstanceState.getString(stateString)
-        textView2.text = mCalorie
-    }
-
-    // метод отвечает за валидацию входных данных
-    private fun validation(): Boolean {
-        return when {
-            // валидация поля роста
-            editTextTextPersonHeight.text.toString().isEmpty() -> {
-                editTextTextPersonHeight.error = resources.getString(R.string.errorHeightNull)
-                false
-            }
-            editTextTextPersonHeight.text.toString().toInt() !in 130..210 -> {
-                editTextTextPersonHeight.error = resources.getString(R.string.errorHeightLimit)
-                false
-            }
-
-            // валидация поля веса
-            editTextTextPersonWeight.text.toString().isEmpty() -> {
-                editTextTextPersonWeight.error = resources.getString(R.string.errorWeightNull)
-                false
-            }
-            editTextTextPersonWeight.text.toString().toInt() !in 30..200 -> {
-                editTextTextPersonWeight.error = ""
-                false
-            }
-
-            // валидация поля возраста
-            editTextTextPersonAge.text.toString().isEmpty() -> {
-                editTextTextPersonAge.error = resources.getString(R.string.errorAgeNull)
-                false
-            }
-            editTextTextPersonAge.text.toString().toInt() !in 1..100 -> {
-                editTextTextPersonAge.error = resources.getString(R.string.errorAgeLimit)
-                false
-            }
-
-            !radioButtonMale.isChecked and !radioButtonFemale.isChecked -> {
-                createCustomToast(resources.getString(R.string.errorChooseGender), Toast.LENGTH_SHORT, Gravity.CENTER, 0,0)
-                false
-            }
-
-            else -> true
-        }
-    }
-
-    private fun getCalculatedCalorie(): String {
-        // константы для расчета BMR по формуле Харриса – Бенедикта
-        val constMale = arrayOf(66.5, 13.75, 5.003, 6.755)
-        val constFemale = arrayOf(655.1, 9.563, 1.85, 4.676)
-
-        val height = editTextTextPersonHeight.text.toString().toInt()
-        val weight = editTextTextPersonWeight.text.toString().toInt()
-        val age = editTextTextPersonAge.text.toString().toInt()
-
-        var pal = 0.0 // уровень активности человека
-        when (spinnerActionLevel.selectedItemId.toInt()) {
-            0 -> pal = 1.2
-            1 -> pal = 1.375
-            2 -> pal = 1.7
-        }
-
-        var calorie = 0.0 // суточная норма калорий
-        // считаю по формуле Харриса – Бенедикта BMR и затем умножаю BMR на PAL
-        when {
-            radioButtonMale.isChecked -> {
-                calorie = (constMale[0] + (constMale[1] * weight) + (constMale[2] * height) - (constMale[3] * age)) * pal
-            }
-            radioButtonFemale.isChecked -> {
-                calorie = (constFemale[0] + (constFemale[1] * weight) + (constFemale[2] * height) - (constFemale[3] * age)) * pal
-            }
-        }
-
-        return calorie.toInt().toString()
-    }
-
-    // Создание кастомного Toast
-    private fun createCustomToast(text: String, toastLength: Int, gravity: Int, xOffset: Int, yOffset: Int) {
-        val customToast = Toast.makeText(this, text, toastLength)
-        customToast.setGravity(gravity, xOffset, yOffset)
-        customToast.show()
+    // создание Drawer для toolbar, использована библиотека mikepenz/MaterialDrawer
+    private fun createDrawer() {
+        DrawerBuilder()
+            .withActivity(this) // привязываем к этой активности
+            .withToolbar(toolbar) // привязываем к тулбару
+            .withActionBarDrawerToggle(true) // добавляем toggle
+            .withSelectedItem(-1) // не выбираем начальный элемент
+            .addDrawerItems( // добавляем пункты меню
+                PrimaryDrawerItem()
+                    .withIdentifier(0)
+                    .withName("Сброс веса"),
+                PrimaryDrawerItem()
+                    .withIdentifier(1)
+                    .withName("Поддержание веса"),
+                PrimaryDrawerItem()
+                    .withIdentifier(2)
+                    .withName("Набор веса")
+            )
+            .withOnDrawerItemClickListener(object: Drawer.OnDrawerItemClickListener { // создаем слушателя для пунктов меню
+                override fun onItemClick(view: View?, position: Int, drawerItem: IDrawerItem<*>): Boolean {
+                        when (position) {
+                        0 -> {
+                            supportFragmentManager
+                                .beginTransaction()
+                                .addToBackStack(null)
+                                .replace(R.id.fr_main, FragmentLosing())
+                                .commit()
+                        }
+                        1 -> {
+                            supportFragmentManager
+                                .beginTransaction()
+                                .addToBackStack(null)
+                                .replace(R.id.fr_main, FragmentMaintaining())
+                                .commit()
+                        }
+                        2 -> {
+                            supportFragmentManager
+                                .beginTransaction()
+                                .addToBackStack(null)
+                                .replace(R.id.fr_main, FragmentGaining())
+                                .commit()
+                        }
+                    }
+                    return false
+                }})
+            .build()
     }
 }
